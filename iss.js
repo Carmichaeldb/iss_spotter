bda9323921a15e01fd7141197827f21943102488
David Carmichael <carmichael.db88@gmail.com>
2023-10-22 00:48:13 +0000

    add if statement if response error occurs, comment out test function in index.js

--- iss.js
+++ iss.js
@@ -13,6 +13,12 @@ const fetchMyIP = function(callback) {
   request(url, (error, response, body) => {
     if (error) {
       callback(console.log(error), null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
     }
     callback(null, JSON.parse(body));
   });
